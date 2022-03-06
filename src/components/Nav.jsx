1815974178f12c24beca15755397bdc3ea005adc
Saoming <Hujanesword@gmail.com>
2022-03-07 04:11:26 +0700

    change this a little bit

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 
 const NavigationMenu = ({showMenu, setShowMenu}) => {
 
+    if(showMenu === false) {
+        return null
+    }
     return (
-        { showMenu ? (  <div>
+        <div>
             <div className="fixed w-full h-full bg-lightDark opacity-30 z-10"></div>
             <nav className="fixed w-1/2 h-full  top-0 right-0 z-50 text-white">
                 hello    
             </nav>
         </div>
-        ) : null};
     )
 }
 
